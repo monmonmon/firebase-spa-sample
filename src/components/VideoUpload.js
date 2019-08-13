5bb301013c4046ebdbbc85d6775943fa020c81b4
Simon Yamada <ymdsmn@gmail.com>
2019-08-13 21:05:39 +0900

    save metadata  of the uploaded video to firestore

--- src/components/VideoUpload.js
+++ src/components/VideoUpload.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/storage'
+import 'firebase/firestore'
+import _ from 'lodash'
 
 class VideoUpload extends Component {
   constructor(props) {
     super(props)
     this.state = { video: null }
   }
-  handleChange(event) {
+  onFileSelect(event) {
     event.preventDefault()
     const video = event.target.files[0]
-    console.log('video:', video)
     this.setState({ video })
   }
-  handleSubmit(event) {
+  onSubmit(event) {
     event.preventDefault()
     this.fileUpload(this.state.video)
   }
@@ -23,19 +24,44 @@ class VideoUpload extends Component {
       const filePath = `videos/${userUid}/${video.name}`
       console.log('filePath:', filePath)
       const videoStorageRef = firebase.storage().ref(filePath)
-      console.log('videoStorageRef:', videoStorageRef)
-      const fileSnapshot = await videoStorageRef.put(video)
+      const idToken = await firebase.auth().currentUser.getIdToken(true)
+      const metadataForStorage = {
+        customMetadata: { idToken }
+      }
+      const fileSnapshot = await videoStorageRef.put(video, metadataForStorage)
       console.log(fileSnapshot)
+
+      if (video.type === 'video/mp4') {
+        // メタデータをFirestoreに保存
+        const downloadURL = await videoStorageRef.getDownloadURL()
+        let metadataForFirestore = _.omitBy(fileSnapshot.metadata, _.isEmpty)
+        metadataForFirestore = Object.assign(metadataForFirestore, { downloadURL })
+        this.saveVideoMetadata(metadataForFirestore)
+      }
+
+      if (fileSnapshot.state === 'success') {
+        this.setState({ video: null })
+      } else {
+        alert('ファイルアップロードに失敗しました')
+      }
     } catch(error) {
       console.log(error)
       return
     }
   }
+  async saveVideoMetadata(metadata) {
+    const userUid = firebase.auth().currentUser.uid
+    const videoRef = firebase.firestore()
+      .doc(`users/${userUid}`)
+      .collection('videos').doc()
+    metadata = Object.assign(metadata, { uid: videoRef.id })
+    await videoRef.set(metadata, { merge: true })
+  }
   render() {
     return (
-      <form onSubmit={ e => this.handleSubmit(e) }>
+      <form onSubmit={ e => this.onSubmit(e) }>
         <h2>Video Upload</h2>
-        <input type="file" accept="video/*" onChange={ e => this.handleChange(e) } />
+        <input type="file" accept="video/*" onChange={ e => this.onFileSelect(e) } />
         <button type="submit">Upload Video</button>
       </form>
     )
