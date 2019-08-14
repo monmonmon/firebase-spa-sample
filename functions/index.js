febda40af332e27bbee8714b4c74f7e5ba04cec2
Simon Yamada <ymdsmn@gmail.com>
2019-08-14 14:22:18 +0900

    implement a firebase function to save user info to firestore when signed up

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,38 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const serviceAccount = require('./config/service_account.json')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://fir-reacty-videos.firebaseio.com',
+  })
+  admin.firestore().settings({ timestampsInSnapshots: true })
+} catch(error) {
+  console.log(error)
+}
+
+const defaultUserIcon = 'https://randomuser.me/api/portraits/med/men/1.jpg'
+
+exports.saveUser = functions.auth.user().onCreate(async user => {
+  try {
+    console.log('saveUser')
+    const result = await admin.firestore().doc(`users/${user.uid}`).create({
+      uid: user.uid,
+      displayName: user.displayName || '名無し',
+      email: user.email,
+      emailVerified: user.emailVerified,
+      photoURL: user.photoURL || defaultUserIcon,
+      phoneNumber: user.phoneNumber,
+      providerData: {
+        providerId: user.providerData.length === 0 ? 'password' : user.providerData[0].providerId,
+        uid: user.providerData.length === 0 ? user.email : user.providerData[0].uid,
+      },
+      disabled: user.disabled,
+    })
+    console.log('result:', result)
+    console.log(`User ${user.uid}, ${user.email} has been saved at ${result.writeTime.toDate()}`)
+  } catch(error) {
+    console.log(error)
+  }
+})
